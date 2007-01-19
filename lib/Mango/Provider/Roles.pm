296d8440508504311a6035394d98ea90672595c6
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2007-01-19 02:40:50 +0000

    Added add_user and user_roles to Roles provider
    git-svn-id: http://svn.icantfocus.com/CPAN/Mango/trunk@1662 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Mango/Provider/Roles.pm
+++ lib/Mango/Provider/Roles.pm
@@ -5,9 +5,61 @@ use warnings;
 
 BEGIN {
     use base qw/Mango::Provider::DBIC/;
+    use Scalar::Util ();
 };
 __PACKAGE__->result_class('Mango::Role');
 __PACKAGE__->source_name('Roles');
+
+*add_user = \&add_users;
+
+sub add_users {
+    my ($self, $role, @users) = @_;
+
+    if (Scalar::Util::blessed $role && $role->isa('Mango::Role')) {
+        $role = $role->id;
+    };
+
+    $role = $self->resultset->single({
+        id => $role
+    });
+
+    foreach my $user (@users) {
+        if (Scalar::Util::blessed $user && $user->isa('Mango::User')) {
+            $user = $user->id;
+        };
+
+        $role->add_to_map_users_roles({
+            user_id => $user
+        });
+    };
+};
+
+sub user_roles {
+    my ($self, $user) = @_;
+
+    if (Scalar::Util::blessed $user && $user->isa('Mango::User')) {
+        $user = $user->id;
+    };
+
+    my @results = map {
+        $self->result_class->new({
+            provider => $self,
+            data => {$_->get_columns}
+        })
+    } $self->resultset->search({
+        'map_users_roles.user_id' => $user
+    }, {
+        join => 'map_users_roles'
+    })->all;
+
+    if (wantarray) {
+        return @results;
+    } else {
+        return Mango::Iterator->new({
+            data => \@results
+        });
+    };
+};
 
 1;
 __END__
