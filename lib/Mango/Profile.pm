63abbd7c7da5da89a15bf7f870c4473ced723c3f
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2007-01-27 18:56:50 +0000

    Added accessors for created()
    git-svn-id: http://svn.icantfocus.com/CPAN/Mango/trunk@1676 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Mango/Profile.pm
+++ lib/Mango/Profile.pm
@@ -6,6 +6,6 @@ use warnings;
 BEGIN {
     use base qw/Mango::Object/;
 };
-__PACKAGE__->mk_group_accessors('column', qw/first_name last_name/);
+__PACKAGE__->mk_group_accessors('column', qw/first_name last_name created/);
 
 1;
--- lib/Mango/User.pm
+++ lib/Mango/User.pm
@@ -6,6 +6,6 @@ use warnings;
 BEGIN {
     use base qw/Mango::Object/;
 };
-__PACKAGE__->mk_group_accessors('column', qw/username password/);
+__PACKAGE__->mk_group_accessors('column', qw/username password created/);
 
 1;
