c0d33aa862b8227f8e2ade4d82803c5e5ef293ee
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2007-01-28 01:43:37 +0000

    Added user_id back to Cart for tracking purposes
    git-svn-id: http://svn.icantfocus.com/CPAN/Mango/trunk@1680 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Mango/Schema/Cart.pm
+++ lib/Mango/Schema/Cart.pm
@@ -18,6 +18,12 @@ __PACKAGE__->add_columns(
         is_auto_increment => 1,
         is_nullable       => 0
     },
+    user_id => {
+        data_type      => 'UINT',
+        is_nullable    => 1,
+        is_foreign_key => 1,
+        default_value  => 0
+    },
     created => {
         data_type     => 'DATETIME',
         is_nullable   => 0
@@ -25,6 +31,9 @@ __PACKAGE__->add_columns(
 );
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->has_many(items => 'Mango::Schema::Cart::Item', {'foreign.cart_id' => 'self.id'});
+__PACKAGE__->might_have(user => 'Mango::Schema::User',
+    {'foreign.id' => 'self.user_id'}
+);
 
 1;
 __END__
