8989abc349fcf6a436ffbf3b5453652ad213bbd3
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2007-02-01 00:30:55 +0000

    Removed auto creation of profile. That will just lead to evil.
    git-svn-id: http://svn.icantfocus.com/CPAN/Mango/trunk@1700 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Mango/Provider/Profiles.pm
+++ lib/Mango/Provider/Profiles.pm
@@ -9,18 +9,5 @@ BEGIN {
 __PACKAGE__->result_class('Mango::Profile');
 __PACKAGE__->source_name('Profiles');
 
-sub get_by_user {
-    my $self = shift;
-    my $object = shift;
-    my $id = Scalar::Util::blessed($object) ? $object->id : $object ;
-
-    my $result = $self->resultset->find_or_create({user_id => $id}, @_);
-
-    return $self->result_class->new({
-        provider => $self,
-        data => {$result->get_columns}
-    });
-};
-
 1;
 __END__
