fc27963306acc2f16399d60dce635f0610c238d2
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2007-02-07 03:13:23 +0000

    Added delete/update/create attributes
    git-svn-id: http://svn.icantfocus.com/CPAN/Mango/trunk@1723 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Mango/Provider/Products.pm
+++ lib/Mango/Provider/Products.pm
@@ -6,10 +6,13 @@ use warnings;
 BEGIN {
     use base qw/Mango::Provider::DBIC/;
     use Mango::Exception ();
+    use Scalar::Util ();
 
     __PACKAGE__->mk_group_accessors('component_class', qw/attribute_class/);
+    __PACKAGE__->mk_group_accessors('inherited', qw/attribute_source_name/);
 };
 __PACKAGE__->attribute_class('Mango::Attribute');
+__PACKAGE__->attribute_source_name('ProductAttributes');
 __PACKAGE__->result_class('Mango::Product');
 __PACKAGE__->source_name('Products');
 
@@ -25,38 +28,38 @@ sub get_by_sku {
     });
 };
 
-sub search {
-    my ($self, $filter, $options) = @_;
+sub get_by_tags {
+    my ($self, @tags) = @_;
+};
 
-    $filter  ||= {};
+sub create_attribute {
+    my ($self, $product, $data) = @_;
+    my $resultset = $self->schema->resultset($self->attribute_source_name);
+
+    if (Scalar::Util::blessed $data && $data->isa('Mango::Attribute')) {
+        $data = {$data->data};
+    };
+    $data->{'product_id'} = $product->id;
+
+    return $resultset->create($data);
+};
+
+sub search_attributes {
+    my ($self, $product, $filter, $options) = @_;
+
+    $filter ||= {};
     $options ||= {};
 
-    my $attributes = delete $options->{'attributes'};
-    if ($attributes) {
-        $options = {%{$options}, join => 'attributes', prefetch => 'attributes'}
-    };
+    $filter->{'product_id'} = $product->id;
 
-    ## yeah, I should fix this later...surprised it works
     my @results = map {
-        $self->result_class->new({
+        $self->attribute_class->new({
             provider => $self,
-            data => {
-                $_->get_inflated_columns,
-                attributes => Mango::Iterator->new(
-                    {data => [
-                        map {
-                            $attributes ?
-                                $self->attribute_class->new({
-                                    provider => $self,
-                                    data => {$_->get_inflated_columns}
-                                })
-                            : ()
-                        } $_->attributes->all
-                    ]}
-                )
-            }
+            data => {$_->get_inflated_columns}
         })
-    } $self->resultset->search($filter, $options)->all;
+    } $self->schema->resultset($self->attribute_source_name)->search(
+        $filter, $options
+    )->all;
 
     if (wantarray) {
         return @results;
@@ -65,6 +68,27 @@ sub search {
             data => \@results
         });
     };
+};
+
+sub delete_attributes {
+    my ($self, $filter, $options) = @_;
+    my $resultset = $self->schema->resultset($self->attribute_source_name);
+
+    $filter ||= {};
+    $options ||= {};
+
+    return $resultset->search(
+        $filter, $options
+    )->all;
+};
+
+sub update_attribute {
+    my ($self, $attribute) = @_;
+    my $resultset = $self->schema->resultset($self->attribute_source_name);
+
+    return $resultset->find($attribute->id)->update(
+        {%{$attribute->data}}
+    );
 };
 
 1;
