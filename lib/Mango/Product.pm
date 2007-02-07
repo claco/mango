4535dded96d9ddaa8fb35e3316cea59c0f35c17e
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2007-02-07 03:12:26 +0000

    Mapped add_attribute/attributes to provider
    git-svn-id: http://svn.icantfocus.com/CPAN/Mango/trunk@1722 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Mango/Product.pm
+++ lib/Mango/Product.pm
@@ -6,7 +6,19 @@ use warnings;
 BEGIN {
     use base qw/Mango::Object/;
 
-    __PACKAGE__->mk_group_accessors('column', qw/sku name description price attributes/);
+    __PACKAGE__->mk_group_accessors('column', qw/sku name description price/);
+};
+
+sub add_attribute {
+    my ($self, $data) = @_;
+
+    return $self->provider->create_attribute($self, $data);
+};
+
+sub attributes {
+    my $self = shift;
+
+    return $self->provider->search_attributes($self, @_);
 };
 
 1;
