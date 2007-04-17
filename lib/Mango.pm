96c1cde201376bc8b0a784865dd114c582dde586
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2007-04-17 01:29:43 +0000

    r157@np5660:  claco | 2007-04-16 21:33:19 -0400  Updated Makefile.PL/Mango with version, requirements
    git-svn-id: http://svn.icantfocus.com/CPAN/Mango/trunk@1804 52a70e3a-55ec-0310-b14e-7917f69ecacc

--- lib/Mango.pm
+++ lib/Mango.pm
@@ -3,7 +3,7 @@ package Mango;
 use strict;
 use warnings;
 
-our $VERSION = '0.01_01';
+our $VERSION = '0.01000_01';
 
 1;
 __END__
@@ -20,6 +20,18 @@ You can get the latest source from the Subversion repository located at
 http://svn.mangoframework.com/CPAN/Mango/.
 
 If you need help or have any questions, stop by #mango@irc.perl.org
+
+B<WARNING: This is an alpha release containing core modules only. No Catalyst
+or web bits are included.>
+
+Mango relies on a versions of Handel and DBIx::Class that are not yet
+released. You will need DBIx::Class-current from:
+
+    http://dev.catalystframework.org/repos/bast/branches/DBIx-Class-current/
+
+and the latest version of Handel from here:
+
+    http://svn.handelframework.com/CPAN/Handel/branches/DBIC-1.0/
 
 =head1 AUTHOR
 
