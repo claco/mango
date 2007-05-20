a05dbf62cc680ce4ce843d1f69f67e148c27d04c
claco <claco@52a70e3a-55ec-0310-b14e-7917f69ecacc>
2007-05-20 03:16:11 +0000

    r226@np5660:  claco | 2007-05-19 23:24:06 -0400  Added ru I18N file, mostly because it's a different codepage to test with
    git-svn-id: http://svn.icantfocus.com/CPAN/Mango/trunk@1865 52a70e3a-55ec-0310-b14e-7917f69ecacc

---
+++ lib/Mango/I18N/ru.pm
@@ -0,0 +1,46 @@
+## no critic
+# $Id$
+package Mango::I18N::ru;
+use strict;
+use warnings;
+use utf8;
+use vars qw/%Lexicon/;
+
+BEGIN {
+    use base qw/Mango::I18N/;
+};
+
+%Lexicon = (
+    Language => 'русском',
+);
+
+1;
+__END__
+
+=head1 NAME
+
+Mango::I18N::ru - Mango Language Pack: Russian
+
+=head1 SYNOPSIS
+
+    use Mango::I18N qw/translate/;
+
+    {
+        local $ENV{'LANG'} = 'ru';
+        print translate('Hello');
+    };
+
+=head1 DESCRIPTION
+
+Mango::I18N::ru contains all of the messages used in Mango in Russian.
+
+=head1 SEE ALSO
+
+L<Mango::I18N>
+
+=head1 AUTHOR
+
+    Christopher H. Laco
+    CPAN ID: CLACO
+    claco@chrislaco.com
+    http://today.icantfocus.com/blog/
